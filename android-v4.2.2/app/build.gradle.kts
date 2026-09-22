efc1688f9df74f8bb59bd1a46a16d2e7505fcb7e
saeid1979 <125009879+saeid1979@users.noreply.github.com>
2026-09-22 11:10:24 +0100

    Android v4.2.2: add build.gradle.kts

---
+++ android-v4.2.2/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "com.javid.downloader"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.javid.downloader"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 4220
+        versionName = "4.2.2"
+    }
+}
