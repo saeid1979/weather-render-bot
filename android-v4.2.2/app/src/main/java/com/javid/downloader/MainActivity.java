1febf98a073f97aff0d97ed79d1ff243f43b1658
saeid1979 <125009879+saeid1979@users.noreply.github.com>
2026-09-22 11:10:33 +0100

    Android v4.2.2: add MainActivity.java

---
+++ android-v4.2.2/app/src/main/java/com/javid/downloader/MainActivity.java
@@ -0,0 +1,15 @@
+package com.javid.downloader;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends Activity {
+ @Override public void onCreate(Bundle b){
+  super.onCreate(b);
+  WebView w=new WebView(this);
+  w.getSettings().setJavaScriptEnabled(true);
+  w.setWebViewClient(new WebViewClient());
+  w.loadUrl("file:///android_asset/index.html");
+  setContentView(w);
+ }
+}
